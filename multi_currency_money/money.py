68c19a6ca3e1e5b8256d8389dd53b8f99fbef80e
Jerry Lu <lreij@163.com>
2012-03-24 20:50:46 +0800

    1. Proceeded in spite of the risk. 2. Reduce coupling between the tests and the code.

--- multi_currency_money/money.py
+++ multi_currency_money/money.py
@@ -8,23 +8,21 @@ import unittest
 class Dollar:
 
     def __init__(self, amount):
-        self.amount = amount 
+        self.__amount = amount 
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Dollar(self.__amount * multiplier)
 
     def __eq__(self, obj):
-        return self.amount == obj.amount
+        return self.__amount == obj.__amount
 
 
 class TestTDD(unittest.TestCase):
 
     def testMultiplication(self):
         five = Dollar(5)
-        product = five.times(2)
-        self.assertEquals(10, product.amount)
-        product = five.times(3)
-        self.assertEquals(15, product.amount)
+        self.assertEquals(Dollar(10), five.times(2))
+        self.assertEquals(Dollar(15), five.times(3))
 
     def testEquality(self):
         self.assertTrue(Dollar(5) == Dollar(5))
