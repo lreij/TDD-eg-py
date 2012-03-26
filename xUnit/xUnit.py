b64839e08afe83fbff49bd948aec5dcf5e0413f2
Jerry Lu <lreij@163.com>
2012-03-26 22:55:45 +0800

    - The tet can be simple if and only if another test is in place and running   correctly.

--- xUnit/xUnit.py
+++ xUnit/xUnit.py
@@ -7,16 +7,20 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
 
-    def __init__(self, name):
+    def setUp(self):
         self.wasRun = None
-        TestCase.__init__(self, name)
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
@@ -24,15 +28,21 @@ class WasRun(TestCase):
 
 class TestCaseTest(TestCase):
 
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
 
 
 def main():
     TestCaseTest("testRunning").run()
+    TestCaseTest("testSetUp").run()
 
 
 if __name__ == '__main__':
