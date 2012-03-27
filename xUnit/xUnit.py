7c8573c9d5d64e9b464454862d84c1f8076cfe37
Jerry Lu <lreij@163.com>
2012-03-27 22:49:30 +0800

    - Make the larger test work quickly using the mechanism demostrated by the   smaller test.

--- xUnit/xUnit.py
+++ xUnit/xUnit.py
@@ -17,8 +17,11 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
         return result
 
@@ -44,12 +47,17 @@ class TestResult:
 
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount += 1
 
+    def testFailed(self):
+        self.errorCount += 1
+
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % \
+        (self.runCount, self.errorCount)
 
 
 class TestCaseTest(TestCase):
@@ -72,11 +80,18 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 1 failed", result.summary)
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
 
 def main():
     TestCaseTest("testTemplateMethod").run()
     TestCaseTest("testResult").run()
     TestCaseTest("testFailedResult").run()
+    TestCaseTest("testFailedResultFormatting").run()
 
 
 if __name__ == '__main__':
